f05dc28ff567815eafc0c8b3c3274cc8e4dc37b3
Diego Araujo <diegolaraujo96@gmail.com>
2023-10-13 13:03:14 -0300

    Lesson 2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +16,32 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(post => {
+            return (
+              <li className={utilStyles.listItem} key={post.id}>
+                {post.title}
+                <br/>
+                {post.id}
+                <br/>
+                {post.date}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
