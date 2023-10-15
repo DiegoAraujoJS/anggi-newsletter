50040368e5d41be1cd6415fe9c2b10dbc1b99866
Diego Araujo <diegolaraujo96@gmail.com>
2023-10-14 22:38:00 -0300

    Centered images on carousel.

--- components/carousel/index.js
+++ components/carousel/index.js
@@ -1,44 +1,20 @@
-import Head from "next/head";
+import Carousel from 'react-bootstrap/Carousel';
 import styles from './carousel.module.css'
 
-export default function Carousel() {
+export default function Component() {
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"/>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"/>
-      </Head>
-      <div id="myCarousel" className={`carousel slide ${styles['carousel']}`}data-bs-ride="carousel">
-        <ol className="carousel-indicators">
-          <li data-bs-target="#myCarousel" data-bs-slide-to="0" className="active"></li>
-          <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
-          <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
-        </ol>
-
-        <div className={`carousel-inner ${styles['carousel-inner']}`}>
-          <div className={`carousel-item active ${styles['carousel-item']} anggiglen`}>
-            <img src="/images/anggi1.jpeg" alt="Image 1"/>
-            <div className="carousel-caption">Caption 1</div>
-          </div>
-          <div className={`carousel-item ${styles['carousel-item']} active`}>
-            <img src="/images/anggi2.jpeg" alt="Image 2"/>
-            <div className="carousel-caption">Caption 2</div>
-          </div>
-          <div className={`carousel-item ${styles['carousel-item']} active`}>
-            <img src="/images/anggi3.jpeg" alt="Image 3"/>
-            <div className="carousel-caption">Caption 3</div>
-          </div>
-        </div>
-
-        <a className="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#myCarousel" role="button" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </a>
-      </div>
+      <Carousel className={styles['carousel']} fade>
+        <Carousel.Item className={styles['carousel-item']}>
+          <img src="/images/anggi1.jpeg" alt="Image 1"/>
+        </Carousel.Item>
+        <Carousel.Item className={styles['carousel-item']}>
+          <img src="/images/anggi2.jpeg" alt="Image 2"/>
+        </Carousel.Item>
+        <Carousel.Item className={styles['carousel-item']}>
+          <img src="/images/anggi3.jpeg" alt="Image 3"/>
+        </Carousel.Item>
+      </Carousel>
     </>
   )
 }
