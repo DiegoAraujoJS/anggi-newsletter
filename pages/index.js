447ea42df5c2165aff97aaaa533d268605f143cb
Diego Araujo <diegolaraujo96@gmail.com>
2023-11-14 22:01:44 -0300

    Added warning modal before updating presentation.

--- pages/index.js
+++ pages/index.js
@@ -36,26 +36,63 @@ export default function Home({allPostsData, presentation}) {
         <title>{siteTitle}</title>
       </Head>
 
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box">
+          Seguro que querés eliminar "{postToDelete.title}"?
+          <div className="modal-action">
+            <button className="btn btn-primary" onClick={() => fetch(`/api/posts/deletePost`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                authorization: localStorage.getItem('adminPassword')
+              },
+              body: JSON.stringify(postToDelete.id)
+            })
+              .then(() => location.reload())            
+            }>Eliminar</button>
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Cerrar</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <div dangerouslySetInnerHTML={{__html: body}}></div>
+          <div className='modal-action'>
+            <button className='btn' onClick={() => {
+              fetch(`/api/presentation`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  authorization: localStorage.getItem('adminPassword')
+                },
+                body: JSON.stringify({body})
+              })
+                .then((res) => {
+                  if (res.status === 200) {
+                    return res.text().then(toast.success)
+                  }
+                })
+                .then(() => setTimeout(() => location.reload(), 2000))
+            }}>Update</button>
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Cerrar</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+
       {isAdmin ? <div>
         <RichTextEditor value={body} handleChange={html => setBody(html)}/>
-        <button className='btn' onClick={() => {
-          fetch(`/api/presentation`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              authorization: localStorage.getItem('adminPassword')
-            },
-            body: JSON.stringify({body})
-          })
-          .then((res) => {
-              if (res.status === 200) {
-                return res.text().then(toast.success)
-              }
-            })
-        }}>Update</button>
+        <button className='btn' onClick={() => document.getElementById('my_modal_3').showModal()}>Update</button>
       </div> : <section className={utilStyles.headingMd}>
           <div dangerouslySetInnerHTML={{__html: body.replace(`target="_blank"`, '')}}></div>
         </section>}
+
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <div className='flex items-center text-2xl w-1/5 justify-between'>
@@ -63,27 +100,7 @@ export default function Home({allPostsData, presentation}) {
           {isAdmin ? <button className="btn btn-circle btn-primary" onClick={() => router.push("/posts/create")}>+</button>
             : null}
         </div>
-        {isAdmin ? <dialog id="my_modal_2" className="modal">
-        <div className="modal-box">
-          Seguro que querés eliminar "{postToDelete.title}"?
-          <div className="modal-action">
-              <button className="btn btn-primary" onClick={() => fetch(`/api/posts/deletePost`, {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                  authorization: localStorage.getItem('adminPassword')
-                },
-                body: JSON.stringify(postToDelete.id)
-              })
-                .then(() => location.reload())            
-              }>Eliminar</button>
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Cerrar</button>
-            </form>
-          </div>
-        </div>
-      </dialog> : null}
+
         <ul className={utilStyles.list}>
           {allPostsData.map(post => {
             return (
