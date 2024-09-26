23e385d40da1e321a768ee7af92eac959f21f8e6
Diego Araujo <diegolaraujo96@gmail.com>
2024-09-26 00:03:35 -0300

    implemento página de admin/subscribers

---
+++ pages/admin/subscribers/index.js
@@ -0,0 +1,57 @@
+import Layout from '../../../components/layout';
+import Head from 'next/head';
+import { getAllSubscriptions } from '../../../lib/database/queries/subscription';
+
+export default function AdminSubscribers({ subscribers }) {
+  return (
+    <Layout>
+      <Head>
+        <title>Admin: Email subscriptos</title>
+      </Head>
+      <h1 className="text-3xl font-bold mb-4">Emails subscriptos</h1>
+      <ul className="list-disc list-inside">
+        {subscribers.map((subscriber, index) => (
+          <li key={index} className="mb-2">
+            {subscriber.email} - Se subscribió el: {subscriber.createdAt}
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+}
+
+function formatDate(date) {
+  if (isNaN(date.getTime())) {
+    return 'Unknown Date';
+  }
+  return `${padZero(date.getDate())}/${padZero(date.getMonth() + 1)}/${date.getFullYear()}`;
+}
+
+function padZero(num) {
+  return num.toString().padStart(2, '0');
+}
+
+export async function getServerSideProps(context) {
+  const { req } = context;
+  const isAdmin = req.cookies.adminPassword === process.env.ADMIN_PASSWORD;
+
+  if (!isAdmin) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const subscribers = await getAllSubscriptions();
+
+  return {
+    props: {
+      subscribers: subscribers.map((subscriber) => ({
+        email: subscriber.email,
+        createdAt: formatDate(subscriber._id.getTimestamp()),
+      })),
+    },
+  };
+}
